8a09fdef2ff8f08c8e222b72745f00f441d5075f
Cuciuc Maxim <maxim.cuciuc123@gmail.com>
2025-09-08 17:15:06 +0300

    added tsx files for poppups

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ModalProvider } from "@/providers/modal-provider";
 
 
 const geistSans = Geist({
@@ -29,7 +30,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      ><ModalProvider />
         {children}
       </body>
     </html>
